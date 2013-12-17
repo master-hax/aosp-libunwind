21738bbfd5a705545d4606cd58e819a34bf67ec8
Kévin PETIT <kevin.petit@arm.com>
2013-12-17 18:17:56 +0000

    Various generic fixes
    A collection of fixes that are architecture-agnostic.
    
    Change-Id: I449bd502584a73ca2b4f004a67f0271c384f66d1
    Signed-off-by: Kévin PETIT <kevin.petit@arm.com>

--- src/elfxx.h
+++ src/elfxx.h
@@ -24,6 +24,9 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 
+#ifndef EFLXX_H
+#define EFLXX_H
+
 #include <fcntl.h>
 #include <unistd.h>
 
@@ -96,3 +99,4 @@ elf_map_image (struct elf_image *ei, const char *path)
 
   return 0;
 }
+#endif // #ifndef EFLXX_H
