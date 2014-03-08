9f639c05fcb23692cebb667950b569a1e178df68
Christopher Ferris <cferris@google.com>
2014-03-07 19:40:06 -0800

    Fix maps caching for local processes.
    It is possible for the map to change while running libunwind. For example,
    if lots of threads are doing local unwinds at the same time. Allow the
    cached map to regenerate when it detects this case.
    
    Included in this change is a refactor of all of the os code so that it can
    also be used to do caching. This is a prelude to being able to attempt to
    push the code upstream.
    
    Also, this moves the code back closer to the original upstream code.
    Hopefully, this will allow me to upstream all of these changes.
    
    Change-Id: Ia219fa61e16e36416133bc95b1dd2161bd5b8ff7

--- src/os-hpux.c
+++ src/os-hpux.c
@@ -31,53 +31,101 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 
 #include "elf64.h"
 
-HIDDEN int
-tdep_get_elf_image (struct elf_image *ei, pid_t pid, unw_word_t ip,
-		    unsigned long *segbase, unsigned long *mapoff,
-		    char *path, size_t pathlen)
-{
-  struct load_module_desc lmd;
-  const char *path2;
+/* ANDROID support update. */
+extern struct map_info *local_map_list;
+HIDDEN define_lock(os_map_lock);
 
-  if (pid != getpid ())
-    {
-      printf ("%s: remote case not implemented yet\n", __FUNCTION__);
-      return -UNW_ENOINFO;
-    }
-
-  if (!dlmodinfo (ip, &lmd, sizeof (lmd), NULL, 0, 0))
-    return -UNW_ENOINFO;
-
-  *segbase = lmd.text_base;
-  *mapoff = 0;			/* XXX fix me? */
-
-  path2 = dlgetname (&lmd, sizeof (lmd), NULL, 0, 0);
-  if (!path2)
-    return -UNW_ENOINFO;
-  if (path)
-    {
-      strncpy(path, path2, pathlen);
-      path[pathlen - 1] = '\0';
-      if (strcmp(path, path2) != 0)
-        Debug(1, "buffer size (%d) not big enough to hold path\n", pathlen);
-    }
-  Debug(1, "segbase=%lx, mapoff=%lx, path=%s\n", *segbase, *mapoff, path);
-
-  return elf_map_image (ei, path);
-}
-
-struct map_info *
-maps_create_list(pid_t pid)
+HIDDEN struct map_info *
+maps_create_list (pid_t pid)
 {
   return NULL;
 }
 
-int maps_is_readable(struct map_info *map_list, unw_word_t addr)
+HIDDEN int
+tdep_get_elf_image (unw_addr_space_t as, pid_t pid, unw_word_t ip)
 {
-  return true;
+  struct load_module_desc lmd;
+  const char *path;
+
+  if (pid != getpid ())
+    {
+      printf ("%s: remote case not implemented yet\n", __FUNCTION__);
+      return NULL;
+    }
+
+  /* First check to see if this ip is in our cache. */
+  map = map_find_from_addr(as->map_list, ip);
+  if (map)
+    return map;
+
+  /* Lock while we update the list. */
+  lock_acquire (&os_map_lock, saved_mask);
+
+  /* Check again if ip is in the map. */
+  map = map_find_from_addr(as->map_list, ip);
+  if (!map)
+    {
+      /* Not in the cache, try and find the data. */
+      if (!dlmodinfo (ip, &lmd, sizeof (lmd), NULL, 0, 0))
+        goto release_lock;
+
+      path = dlgetname (&lmd, sizeof (lmd), NULL, 0, 0);
+      if (!path)
+        goto release_lock;
+
+      map = mempool_alloc (&map_pool);
+      if (map)
+        {
+          map->start = lmd.text_base;
+          map->end = cur_map->start + lmd.text_size;
+          map->offset = 0;			/* XXX fix me? */
+          map->flags = ;
+          map->path = strdup(path2);
+          mutex_init (&cur_map->ei_lock);
+          map->ei.size = 0;
+          map->ei.image = NULL;
+          map->ei_shared = 0;
+          Debug(1, "segbase=%lx, mapoff=%lx, path=%s\n",
+                map->start, map->offset, map->path);
+
+          if (elf_map_cached_image (map, ip) < 0)
+            {
+              free(map);
+              map = NULL;
+            }
+          else
+            {
+              /* Add this element into list in descending order by start. */
+              struct map_info *map_list = as->map_list;
+              if (as->map_list == NULL || map->start > as->map_list->start)
+                {
+                  map->next = as->map_list;
+                  as->map_list = map;
+                }
+              else
+                {
+                  while (map_list->next != NULL && map->start <= map_list->next->start)
+                    map_list = map_list->next;
+                  map->next = map_list->next;
+                  map_list->next = map;
+                }
+            }
+        }
+    }
+release_lock:
+  lock_release (&os_map_lock, saved_mask);
+  return map;
 }
 
-int maps_is_writable(struct map_info *map_list, unw_word_t addr)
+PROTECTED int
+maps_is_local_readable(struct map_info *map_list, unw_word_t addr)
 {
-  return true;
+  return 1;
 }
+
+PROTECTED int
+maps_is_local_writable(struct map_info *map_list, unw_word_t addr)
+{
+  return 1;
+}
+/* End of ANDROID update. */
